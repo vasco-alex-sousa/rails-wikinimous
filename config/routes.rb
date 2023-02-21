29115953be41066e6271f85b53df7d97020aeae8
vasco sousa <vasco.alex.sousa@gmail.com>
2023-02-21 15:55:28 +0000

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/articles', to: 'articles#index'
+  get '/articles/new', to: 'articles#new', as: 'new_article'
+  post '/articles', to: 'articles#create'
+  get '/articles/:id', to: 'articles#show', as: 'article'
+  get '/articles/:id/edit', to: 'articles#edit', as: 'edit_article'
+  delete '/articles/:id', to: 'articles#destroy'
+  patch 'articles/:id', to: 'articles#update', as: 'update_article'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraphs(number: rand(1..4)).join('\n') + "\n\n" + Faker::Lorem.paragraphs(number: rand(1..3)).join('\n')
+  )
+end
